367bd3bf1374112e0fc5dd3dfc40cb3fe9fbb749
mahfuzon <mahfuzon0@gmail.com>
2020-11-26 22:49:34 +0700

    Desain detail produk part 1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,22 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = "Running";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="ParentBox">
+      <div class="Foto">
+        <center><img src="download.jpg"/></center>
+      </div>
+      <div class="Deskripsi">
+        <p class="Cate">{title}</p>
+        <h1 class="Title">Sneaker Hitam</h1>
+        <p class="Price">IDR 73.000</p>
+        <p class="Info">ashdgshdv hdvhgsvdhsgvh havdhgsdvhs hvdghsdvsv hvdhgsvdhs vdshvxs
+        cbdhcvdghvchd sdvsgdvhsgv sdgvsghdv hjvhsvdc hvdhgsvds hvscsvc hsvcgsvc
+        vdsvdghsdvx sdhsvdghs hjvscsvc shcvsvcghsv sghvcgsvc ghsvcgsvc ghsvchgsv ghvschsv</p>
+      </div>
     </div>
   );
 }
