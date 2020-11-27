b1e5479416624a40d9f9d680f674db559dc2e604
mahfuzon <mahfuzon0@gmail.com>
2020-11-27 10:46:49 +0700

    Slicing Component & Props Component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,22 +2,34 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const title = "Running";
   return (
     <div class="ParentBox">
-      <div class="Foto">
-        <center><img src="download.jpg"/></center>
-      </div>
-      <div class="Deskripsi">
-        <p class="Cate">{title}</p>
-        <h1 class="Title">Sneaker Hitam</h1>
-        <p class="Price">IDR 73.000</p>
-        <p class="Info">ashdgshdv hdvhgsvdhsgvh havdhgsdvhs hvdghsdvsv hvdhgsvdhs vdshvxs
-        cbdhcvdghvchd sdvsgdvhsgv sdgvsghdv hjvhsvdc hvdhgsvds hvscsvc hsvcgsvc
-        vdsvdghsdvx sdhsvdghs hjvscsvc shcvsvcghsv sghvcgsvc ghsvcgsvc ghsvchgsv ghvschsv</p>
-      </div>
+      <Produkfoto></Produkfoto>
+      <ProdukInfo name="Adidas Hitam Sneaker yahaha" title="Sneaker"></ProdukInfo>
     </div>
   );
+}
+
+function Produkfoto() {
+  return (
+    <div class="Foto">
+      <center><img src="download.jpg" /></center>
+    </div>
+  );
+}
+
+function ProdukInfo(props) {
+  const {title,name} = props;
+  return (
+    <div class="Deskripsi">
+      <p class="Cate">{title}</p>
+      <h1 class="Title">{name}</h1>
+      <p class="Price">IDR 73.000</p>
+      <p class="Info">ashdgshdv hdvhgsvdhsgvh havdhgsdvhs hvdghsdvsv hvdhgsvdhs vdshvxs
+      cbdhcvdghvchd sdvsgdvhsgv sdgvsghdv hjvhsvdc hvdhgsvds hvscsvc hsvcgsvc
+      vdsvdghsdvx sdhsvdghs hjvscsvc shcvsvcghsv sghvcgsvc ghsvcgsvc ghsvchgsv ghvschsv
+    </p>
+    </div>);
 }
 
 export default App;
